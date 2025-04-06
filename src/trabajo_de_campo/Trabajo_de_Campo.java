887fe4169bb83f0a95a7feaa724b68206bf96bd5
alex-san <sanchezalexsan12@gmail.com>
2025-04-06 15:33:09 -0500

    Se agrega mensaje en el main

--- src/trabajo_de_campo/Trabajo_de_Campo.java
+++ src/trabajo_de_campo/Trabajo_de_Campo.java
@@ -15,6 +15,8 @@ public class Trabajo_de_Campo {
      */
     public static void main(String[] args) {
         System.out.print("Hello");
+        System.out.print("Este es un mensaje nuevo");
+        
     }
     
 }
